24f27834e9d521c48f380af43573dea5ec461e54
Vytautas <likasvy@gmail.com>
2026-06-15 19:07:39 +0100

    Add mobile room entry controls

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,16 +1,26 @@
-import { ChangeEventHandler, KeyboardEventHandler } from 'react';
+import { ChangeEventHandler, InputHTMLAttributes, KeyboardEventHandler } from 'react';
 import styles from './TextInput.module.scss';
 
-interface TextInputProps {
+interface TextInputProps extends Pick<InputHTMLAttributes<HTMLInputElement>, 'placeholder' | 'maxLength' | 'autoComplete'> {
   value?: string;
   onChange?: ChangeEventHandler<HTMLInputElement>;
   onKeyDown?: KeyboardEventHandler<HTMLInputElement>;
+  large?: boolean;
 }
 
-function TextInput({ value, onChange, onKeyDown }: TextInputProps) {
+function TextInput({ value, onChange, onKeyDown, placeholder, maxLength, autoComplete, large }: TextInputProps) {
   return (
-    <div className={styles['input-wrapper']}>
-      <input type="text" className={styles['text-input']} value={value} onChange={onChange} onKeyDown={onKeyDown} />
+    <div className={`${styles['input-wrapper']} ${large ? styles.large : ''}`.trim()}>
+      <input
+        type="text"
+        className={styles['text-input']}
+        value={value}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        placeholder={placeholder}
+        maxLength={maxLength}
+        autoComplete={autoComplete}
+      />
     </div>
   );
 }
